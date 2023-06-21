e7a6cb5b9146c5803f116a76ed367eb7375b18d1
KulikovskyRA <R.A.Kulikovsky@yandex.ru>
2023-06-21 16:18:19 +0300

    aaaaaa

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const render = require('../render');
 
 const router = express.Router();
@@ -12,8 +13,34 @@ const ErrorPage = require('../views/ErrorPage');
 
 const { User, Favor, Way, Review } = require('../../db/models');
 
-router.get('/', (req, res) => {
-  render(Main, { user: req.session.user }, res);
+router.get('/', async (req, res) => {
+  const approved = await Way.findAll({
+    raw: true,
+    nest: true,
+    where: { approved: true },
+  });
+
+  const ratings = await Review.findAll({
+    raw: true,
+    nest: true,
+  });
+
+  approved.map((a) => {
+    const ratingsA = ratings.filter((el) => el.way_id === a.id);
+
+    let scoreA = 0;
+    ratingsA.forEach((rating) => {
+      scoreA += rating.score;
+    });
+    let avrA;
+    ratingsA.length ? (avrA = scoreA / ratingsA.length) : (avrA = 0);
+    a.avr = avrA;
+    return a;
+  });
+
+  approved.sort((a, b) => b.avr - a.avr);
+
+  render(Main, { user: req.session.user, approved }, res);
 });
 
 router.get('/error', (req, res) => {
@@ -34,6 +61,20 @@ router.get('/error', (req, res) => {
   }
   // console.log(req.query);
   render(ErrorPage, { errorSMS }, res);
+});
+
+router.delete('/admin', async (req, res) => {
+  const { id } = req.body;
+
+  try {
+    const deleteOne = await Way.findByPk(id);
+    await deleteOne.destroy();
+
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(404);
+  }
 });
 
 module.exports = router;
