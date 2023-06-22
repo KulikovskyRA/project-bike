15a1c832a3ef873a4ab8e84532e72a03140940f5
Artem <4453735@gmail.com>
2023-06-22 13:23:57 +0400

    Add route on map

--- src/views/DescriptionRoute.jsx
+++ src/views/DescriptionRoute.jsx
@@ -8,14 +8,14 @@ module.exports = function DescriptionRoute({ user, oneWay, reviews }) {
       <div>
         Длина маршрута: {oneWay.way_length} Подтягивается из api Значит, должно
         притягиваться из api и отображаться в бд???
-        <br />И думаю, что нужно ещё одна таблица для хранения точек назначения
+        <br/>И думаю, что нужно ещё одна таблица для хранения точек назначения
         маршрута
       </div>
       <div>Автор маршрута: {oneWay.User.username}</div>
       <div>Населенный пункт: {oneWay.city} Подтягивается из api</div>
       {/* автора маршрута можно узнать только через where по юзер id */}
       {/* оценка должна быть с помощью img и заполняться (менять цвет) */}
-      <div>Оценка </div>
+      <div>Оценка</div>
       <input
         type="checkbox"
         checked="checked"
@@ -36,7 +36,8 @@ module.exports = function DescriptionRoute({ user, oneWay, reviews }) {
           strokeLinejoin="round"
           className="feather feather-heart"
         >
-          <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+          <path
+            d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
         </svg>
         <div className="action">
           <span className="option-1">Добавить в избранное</span>
@@ -45,14 +46,14 @@ module.exports = function DescriptionRoute({ user, oneWay, reviews }) {
       </label>
       <div>
         Оцените приложения
-        <br />! выводится список звездочек или колёс при нажатии на них
+        <br/>! выводится список звездочек или колёс при нажатии на них
         предлагается оставить отзыв, если пользователь не хочет оставлять отзыв,
         то можно просто отправить оценку
       </div>
-
+      
       <button id="buttonLeaveFeedback">
         Оставить отзыв
-        <br />
+        <br/>
         Можно отправить пустой отзыв, но нельзя отправить без оценки
       </button>
       {/* Должен быть список отзывов */}
@@ -71,7 +72,7 @@ module.exports = function DescriptionRoute({ user, oneWay, reviews }) {
         Отзывы:
         <ul>
           {/* список отзывов */}
-
+          
           {reviews.map((review) => (
             <>
               <li key={review.id}>
@@ -86,7 +87,7 @@ module.exports = function DescriptionRoute({ user, oneWay, reviews }) {
       </div>
       {/* здесь должен отображаться маршрут, который уже построен и его можно просмотреть, приблизившись на карте */}
       <div id="YMapsID"></div>
-      <script defer type="text/javascript" src="/js/maps.js" />
+      <script defer type="text/javascript" src="/js/getRoadOnMap.js"/>
     </Layout>
   );
 };
