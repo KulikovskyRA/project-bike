b18436e9ba064f28fc79bfb598745687e0580ef3
KulikovskyRA <R.A.Kulikovsky@yandex.ru>
2023-06-23 10:16:26 +0300

    add admin button to layout

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -17,34 +17,96 @@ module.exports = function Layout({ children, title, user }) {
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <button type="button" className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                <button
+                  type="button"
+                  className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                  aria-controls="mobile-menu"
+                  aria-expanded="false"
+                >
                   <span className="sr-only">Open main menu</span>
-                  <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                  <svg
+                    className="block h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    />
                   </svg>
-                  <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  <svg
+                    className="hidden h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
                   </svg>
                 </button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-end">
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
-                    <a href="/" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Главная</a>
-                    {
-                      user?.username
-                        ? (
-                          <>
-                            <a href="/profile/myway" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">{user.username}</a>
-                            <a href="/users/logout" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Выйти</a>
-                          </>
-                        ) : (
-                          <>
-                            <a href="/users/register" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Регистрация</a>
-                            <a href="/users/login" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Вход</a>
-                          </>
-                        )
-                    }
+                    <a
+                      href="/"
+                      className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                      aria-current="page"
+                    >
+                      Главная
+                    </a>
+
+                    {user?.admin === true ? (
+                      <span>
+                        <a className="navbar-brand" href="/admin">
+                          Проверка маршрутов
+                        </a>
+                        <span className="navbar-brand"> | </span>
+                      </span>
+                    ) : (
+                      <div />
+                    )}
+
+                    {user?.username ? (
+                      <>
+                        <a
+                          href="/profile/myway"
+                          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                        >
+                          {user.username}
+                        </a>
+                        <a
+                          href="/users/logout"
+                          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                        >
+                          Выйти
+                        </a>
+                      </>
+                    ) : (
+                      <>
+                        <a
+                          href="/users/register"
+                          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                        >
+                          Регистрация
+                        </a>
+                        <a
+                          href="/users/login"
+                          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                        >
+                          Вход
+                        </a>
+                      </>
+                    )}
                   </div>
                 </div>
               </div>
@@ -54,9 +116,7 @@ module.exports = function Layout({ children, title, user }) {
             </div>
           </div>
         </nav>
-        <div classNameName="bg-gray-600">
-          {children}
-        </div>
+        <div classNameName="bg-gray-600">{children}</div>
       </body>
     </html>
   );
