dd795552401cf2566d666ab81abf3ff17c019591
KulikovskyRA <R.A.Kulikovsky@yandex.ru>
2023-06-24 20:02:49 +0300

    adminPage

--- src/views/Admin.jsx
+++ src/views/Admin.jsx
@@ -4,34 +4,50 @@ const Layout = require('./Layout');
 module.exports = function Admin({ user, notApproved }) {
   return (
     <Layout user={user}>
-      <div className="cont">
-        {notApproved.map((way) => (
-          <div className="card" id={way.id}>
-            <a href={`/description/${way.id}`}>
-              <h5>{way.title}</h5>
-              <img src={`/images/${way.picture_data}`} alt={`${way.title}`} />
-            </a>
-            <div>
-              {way.city} {'✮'.repeat(Math.round(way.avr))}
-              {'☆'.repeat(Math.floor(5 - way.avr))}
-            </div>
-            <div>
-              {way.way_length}
-              <span> км</span>
-            </div>
-            {/* <div>{way.way_data}</div> */}
-            <a href={`/description/${way.id}`}>Следуй за мной</a>
-            <div>
-              <button type="button" className="adminDel">
-                Удалить
-              </button>
-              <button type="button" className="adminAppr">
-                Одобрить
-              </button>
-            </div>
+      <div className="upper">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 ">
+          <h2 className="sr-only">Products</h2>
+
+          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 text-center cont">
+            {notApproved.map((way) => (
+              <div className="card bg-white p-4" id={way.id}>
+                <a href={`/description/${way.id}`} className="group">
+                  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                    <img
+                      src={`/images/${way.picture_data}`}
+                      alt={`${way.title}`}
+                      className="h-full w-full object-cover object-center group-hover:opacity-75"
+                    />
+                  </div>
+                  <h3 className="text-lg font-medium text-gray-900">
+                    {way.title}
+                  </h3>
+                  <p className="text-sm text-gray-700">{way.city}</p>
+                  <p className="mt-2 text-sm text-gray-700">
+                    {way.way_length}км
+                  </p>
+                  <div>
+                    <button
+                      type="button"
+                      className="adminDel flex w-full justify-center rounded-md bg-gray-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-2"
+                    >
+                      Удалить
+                    </button>
+
+                    <button
+                      type="button"
+                      className="adminAppr flex w-full justify-center rounded-md bg-gray-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-2"
+                    >
+                      Одобрить
+                    </button>
+                  </div>
+                </a>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
+
       <script defer src="/js/main.js" />
     </Layout>
   );
